10db990830b7452dcc97df25737612daf55d1277
Txuraz <txuraz6@gmail.com>
2023-05-23 20:25:56 +0545

    vote API is added

--- article/serializers.py
+++ article/serializers.py
@@ -5,4 +5,4 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = '__all__'
+        fields = ['id', 'title', 'content', 'author', 'created_at']
