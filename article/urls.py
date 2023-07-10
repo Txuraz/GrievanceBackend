48decd307641988f727fc5ecfcaa0839e71a61eb
Txuraz <txuraz6@gmail.com>
2023-07-10 20:33:39 +0545

    Incomplete Article Api and All Article API created

--- article/urls.py
+++ article/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import ListArticles, RetrieveArticle, CreateArticle, DeleteArticle, VoteArticle, SimilarArticles, \
-    ArticleStatusUpdate, CompletedArticleList
+    ArticleStatusUpdate, CompletedArticleList, IncompleteArticles
 
 urlpatterns = [
-    path('articles/', ListArticles.as_view(), name='list_articles'),
+    path('articles/', IncompleteArticles.as_view(), name='incomplete_articles'),
     path('articles/create/', CreateArticle.as_view(), name='create_article'),
     path('articles/<int:article_id>/delete/', DeleteArticle.as_view(), name='delete_article'),
     path('articles/<int:article_id>/vote/', VoteArticle.as_view(), name='vote_article'),
@@ -12,5 +12,6 @@ urlpatterns = [
     path('articles/<int:article_id>/similar/', SimilarArticles.as_view(), name='similar_articles'),
     path('articles/<int:article_id>/status/', ArticleStatusUpdate.as_view(), name='update-article-status'),
     path('articles/completed-articles/', CompletedArticleList.as_view(), name='completed_articles'),
+    path('articles/all/', ListArticles.as_view(), name='all_articles'),
 
 ]
